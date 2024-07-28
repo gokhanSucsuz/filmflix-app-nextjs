79472217785983a8cc32dcfba70d377670699220
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-07-28 14:31:20 +0300

    nextauthOptions file is created

--- app/api/[...nextauth].ts
+++ app/api/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { AuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from "@/libs/prismadb";
 import { compare } from "bcrypt";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export const authOptions: AuthOptions = {
 	providers: [
@@ -45,5 +46,14 @@ export const authOptions: AuthOptions = {
 				return user;
 			}
 		})
-	]
+	],
+	pages: {
+		signIn: "/auth"
+	},
+	debug: process.env.NODE_ENV === "development",
+	adapter: PrismaAdapter(prismadb),
+	session: { strategy: "jwt" },
+	jwt: { secret: process.env.NEXTAUTH_JWT_SECRET },
+	secret: process.env.NEXTAUTH_SECRET
 };
+export default NextAuth(authOptions);
